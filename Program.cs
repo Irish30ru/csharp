80c17bebae3cfa38d3c97438d26864e2345ab67f
Andrey <akokinn2003@yandex.ru>
2023-01-15 12:00:05 +0400

    Исправил четвертое задание

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
 
 Console.Write("Введите значение: ");
 int N = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i < N; i++)
+for (int i = 1; i <= N; i++)
     if (i%2==0)
         Console.WriteLine(i);
     
